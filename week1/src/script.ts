a6ae6ee007f4734e8911fc85d2cb041e5205943d
skyhjune <skyhjune@naver.com>
2026-03-18 21:21:10 +0900

    FEAT: 1주차 mission1 구현

---
+++ week1/src/script.ts
@@ -0,0 +1,59 @@
+const input :HTMLInputElement | null = document.querySelector("input");
+const todo :HTMLElement | null = document.querySelector(".todo");
+const done :HTMLElement | null = document.querySelector(".done");
+
+function addItem(e: KeyboardEvent): void {
+    if (e.key === "Enter"&&input&&todo) {
+        const text = input.value.trim();
+
+        const item = document.createElement("div");
+        const span = document.createElement("span");
+        const button = document.createElement("button");
+
+        item.style.marginBottom = "5px";
+        span.style.marginRight = "10px";
+        span.textContent = text;
+        button.textContent = "완료";
+
+        item.appendChild(span);
+        item.appendChild(button);
+        todo.appendChild(item);
+
+        input.value = "";
+
+    }
+}
+if(input) input.addEventListener("keydown", addItem);
+if(todo) {
+    todo.addEventListener("click", (e:MouseEvent) => {
+        const target = e.target;
+        if(target instanceof HTMLButtonElement && done) {
+            const item = target.parentElement;
+            if(!item) return;
+            const lastElementChild = item.lastElementChild;
+            if(!lastElementChild) return;
+            item.remove();
+            lastElementChild.textContent = "삭제";
+            done.appendChild(item);
+        }
+                
+
+    })
+
+}
+
+if(done) {
+    done.addEventListener("click", (e:MouseEvent) => {
+        const target = e.target;
+        if(target instanceof HTMLButtonElement) {
+            const item = target.parentElement;
+            if(!item) return;
+            item.remove();
+        }
+
+    })
+
+}
+
+
+        
